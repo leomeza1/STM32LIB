6b6c7401dd6540ff51e503348d69115f1d42fce1
Leo Meza <leo.e.meza@gmail.com>
2023-11-08 11:46:48 -0700

    Modify the GpioPin class to use HAL
    Previously, the GpioPin class was implemented without regard
    to the hardware abstraction layer (HAL), so configuration of a
    GPIO pin was being done manually, without the use of HAL. Now,
    the GpioPin class uses HAL to configure and manipulate the
    GPIO pins.

--- Inc/GpioPin.hpp
+++ Inc/GpioPin.hpp
@@ -1,71 +1,14 @@
 #ifndef GPIOPIN_HPP_
 #define GPIOPIN_HPP_
 
+
 #include "stm32f0xx_hal.h"
-
-
-// In the GPIOx_BSRR register, the reset bits (BRy) start at bit 16
-#define GPIOx_BSRR_BR_OFFSET (16)
-
-
-typedef enum {
-	AHB_PCLOCK_DMA   = 0,
-	AHB_PCLOCK_SRAM  = 1,
-	AHB_PCLOCK_FLITF = 2,
-	AHB_PCLOCK_CRC   = 3,
-	AHB_PCLOCK_IOPA  = 4,
-	AHB_PCLOCK_IOPB  = 5,
-	AHB_PCLOCK_IOPC  = 6,
-	AHB_PCLOCK_IOPD  = 7,
-	AHB_PCLOCK_IOPF  = 8
-
-} AHB_Peripheral_Clock_t;
-
-typedef enum {
-	GPIO_MODER_INPUT     = 0,  // 00: Input mode (reset state)
-	GPIO_MODER_OUTPUT    = 1,  // 01: General purpose output mode
-	GPIO_MODER_ALTERNATE = 2,  // 10: Alternate function mode
-	GPIO_MODER_ANALOG    = 3   // 11: Analog mode
-
-} GPIO_Mode_t;
-
-typedef enum {
-	GPIO_OUTPUT_TYPE_PP = 0,  // 0: Output push-pull (reset state)
-	GPIO_OUTPUT_TYPE_OD = 1   // 1: Output open-drain
-
-} GPIO_Output_Type_t;
-
-typedef enum {
-	GPIO_OUTPUT_SPEED_LOW    = 0,  // x0: Low speed
-	GPIO_OUTPUT_SPEED_MEDIUM = 1,  // 01: Medium speed
-	GPIO_OUTPUT_SPEED_HIGH   = 2   // 11: High speed
-
-} GPIO_Output_Speed_t;
-
-typedef enum {
-	GPIO_PUPD_NONE      = 0,  // 00: No pull-up, pull-down
-	GPIO_PUPD_PULL_UP   = 1,  // 01: Pull-up
-	GPIO_PUPD_PULL_DOWN = 2,  // 10: Pull-down
-	GPIO_PUPD_RESERVED  = 3   // 11: Reserved
-
-} GPIO_Pull_Up_Pull_Down_t;
-
-typedef struct {
-	GPIO_TypeDef             *port;
-	uint16_t                  pin;
-	GPIO_Mode_t               mode;
-	AHB_Peripheral_Clock_t    ahb_pclock;
-	GPIO_Output_Type_t        otype;
-	GPIO_Output_Speed_t       ospeed;
-	GPIO_Pull_Up_Pull_Down_t  pupd;
-
-} GPIO_Config_t;
 
 
 class GpioPin
 {
 public:
-    GpioPin(const GPIO_Config_t *gpio_cfg);   // Constructor
+    GpioPin(GPIO_TypeDef *GPIOx, GPIO_InitTypeDef *GPIO_Init);   // Constructor
 
     bool Read();
     void Set();
@@ -74,13 +17,12 @@ public:
 
 private:
 
-    GPIO_TypeDef             *port;
-    uint16_t                  pin;
-    AHB_Peripheral_Clock_t    ahb_pclock;
-    GPIO_Mode_t               mode;
-    GPIO_Output_Type_t        otype;
-    GPIO_Output_Speed_t       ospeed;
-    GPIO_Pull_Up_Pull_Down_t  pupd;
+    GPIO_TypeDef *port;
+    uint32_t      pin;
+    uint32_t      mode;
+    uint32_t      pull;
+    uint32_t      speed;
+    uint32_t      alternate;
 
     void moder_configure(void);
     void ahb_peripheral_clock_enable(void);
--- Src/GpioPin.cpp
+++ Src/GpioPin.cpp
@@ -1,25 +1,46 @@
 
 #include "GpioPin.hpp"
 
-GpioPin::GpioPin(const GPIO_Config_t *gpio_cfg)
+
+GpioPin::GpioPin(GPIO_TypeDef *GPIOx, GPIO_InitTypeDef *GPIO_Init)
     :
-    port(gpio_cfg->port),
-    pin(gpio_cfg->pin),
-	ahb_pclock(gpio_cfg->ahb_pclock),
-	mode(gpio_cfg->mode),
-	otype(gpio_cfg->otype),
-	ospeed(gpio_cfg->ospeed),
-	pupd(gpio_cfg->pupd)
+    port(GPIOx),
+    pin(GPIO_Init->Pin),
+	mode(GPIO_Init->Mode),
+	pull(GPIO_Init->Pull),
+	speed(GPIO_Init->Speed),
+	alternate(GPIO_Init->Alternate)
 {
-	ahb_peripheral_clock_enable();
+    // Enable the GPIO port clock
+    if(GPIOx == GPIOA)
+    {
+        RCC->AHBENR |= RCC_AHBENR_GPIOAEN;
+    }
+    else if(GPIOx == GPIOB)
+    {
+        RCC->AHBENR |= RCC_AHBENR_GPIOBEN;
+    }
+    else if(GPIOx == GPIOC)
+    {
+        RCC->AHBENR |= RCC_AHBENR_GPIOCEN;
+    }
+    else if(GPIOx == GPIOD)
+    {
+        RCC->AHBENR |= RCC_AHBENR_GPIODEN;
+    }
+    else if(GPIOx == GPIOF)
+    {
+        RCC->AHBENR |= RCC_AHBENR_GPIOFEN;
+    }
 
-	moder_configure();
+    // If this pin is configured for output, reset the output level
+    if(GPIO_Init->Mode & MODE_OUTPUT)
+    {
+        HAL_GPIO_WritePin(GPIOx, GPIO_Init->Pin, GPIO_PIN_RESET);
+    }
 
-	output_type_configure();
-
-	output_speed_configure();
-
-	pull_up_pull_down_configure();
+    // Just use HAL to initialize the GPIO pin
+    HAL_GPIO_Init(GPIOx, GPIO_Init);
 }
 
 bool GpioPin::Read()
@@ -29,227 +50,19 @@ bool GpioPin::Read()
 
 void GpioPin::Set()
 {
-    //HAL_GPIO_WritePin(port, pin, GPIO_PIN_SET);
+    HAL_GPIO_WritePin(port, pin, GPIO_PIN_SET);
 
-	port->BSRR |= (1U << pin);
+	//port->BSRR |= (1U << pin);
 }
 
 void GpioPin::Clear()
 {
-    //HAL_GPIO_WritePin(port, pin, GPIO_PIN_RESET);
+    HAL_GPIO_WritePin(port, pin, GPIO_PIN_RESET);
 
-	port->BSRR |= (1U << (pin + GPIOx_BSRR_BR_OFFSET));
+	//port->BSRR |= (1U << (pin + GPIOx_BSRR_BR_OFFSET));
 }
 
 void GpioPin::Toggle()
 {
-    HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_6);
-}
-
-void GpioPin::moder_configure(void)
-{
-	uint32_t register_offset = 0;
-
-	if(pin < 16)
-	{
-		// Get the register bit offset for the specified pin. This will be
-		// used to set the appropriate bits for the specified pin.
-		// Reference section 8.4.1 "GPIO port mode register" for details.
-		register_offset = pin * 2;
-
-		switch(mode)
-		{
-		case GPIO_MODER_INPUT:
-			// 00: Input mode (reset state)
-			port->MODER &= ~(0x3UL << register_offset);
-			break;
-
-		case GPIO_MODER_OUTPUT:
-			// 01: General purpose output mode
-			port->MODER |= (0x1UL << register_offset);
-			port->MODER &= ~(0x2UL << register_offset);
-			break;
-
-		case GPIO_MODER_ALTERNATE:
-			// 10: Alternate function mode
-			port->MODER |= (0x2UL << register_offset);
-			port->MODER &= ~(0x1UL << register_offset);
-			break;
-
-		case GPIO_MODER_ANALOG:
-			// 11: Analog mode
-			port->MODER |= (0x3UL << register_offset);
-			break;
-
-		default:
-			// TODO: Add error handling
-			break;
-		}
-	}
-	else
-	{
-		// TODO: Add error handling
-	}
-}
-
-void GpioPin::ahb_peripheral_clock_enable(void)
-{
-	switch(ahb_pclock)
-	{
-	case AHB_PCLOCK_DMA:
-		RCC->AHBENR |= RCC_AHBENR_DMAEN;
-		break;
-
-	case AHB_PCLOCK_SRAM:
-		RCC->AHBENR |= RCC_AHBENR_SRAMEN;
-		break;
-
-	case AHB_PCLOCK_FLITF:
-		RCC->AHBENR |= RCC_AHBENR_FLITFEN;
-		break;
-
-	case AHB_PCLOCK_CRC:
-		RCC->AHBENR |= RCC_AHBENR_CRCEN;
-		break;
-
-	case AHB_PCLOCK_IOPA:
-		RCC->AHBENR |= RCC_AHBENR_GPIOAEN;
-		break;
-
-	case AHB_PCLOCK_IOPB:
-		RCC->AHBENR |= RCC_AHBENR_GPIOBEN;
-		break;
-
-	case AHB_PCLOCK_IOPC:
-		RCC->AHBENR |= RCC_AHBENR_GPIOCEN;
-		break;
-
-	case AHB_PCLOCK_IOPD:
-		RCC->AHBENR |= RCC_AHBENR_GPIODEN;
-		break;
-
-	case AHB_PCLOCK_IOPF:
-		RCC->AHBENR |= RCC_AHBENR_GPIOFEN;
-		break;
-
-	default:
-		// TODO: Add error handling
-		break;
-	}
-}
-
-void GpioPin::output_type_configure(void)
-{
-	uint32_t register_offset = 0;
-
-	if(pin < 16)
-	{
-		// Get the register bit offset for the specified pin. This will be
-		// used to set the appropriate bits for the specified pin.
-		// Reference section 8.4.2 "GPIO port output type register" for details.
-		register_offset = pin;
-
-		switch(otype)
-		{
-		case GPIO_OUTPUT_TYPE_PP:
-			// 0: Output push-pull (reset state)
-			port->OTYPER &= ~(0x1UL << register_offset);
-			break;
-
-		case GPIO_OUTPUT_TYPE_OD:
-			// 1: Output open-drain
-			port->OTYPER |= (0x1UL << register_offset);
-			break;
-
-		default:
-			// TODO: Add error handling
-			break;
-		}
-	}
-	else
-	{
-		// TODO: Add error handling
-	}
-}
-
-void GpioPin::output_speed_configure(void)
-{
-	uint32_t register_offset = 0;
-
-	if(pin < 16)
-	{
-		// Get the register bit offset for the specified pin. This will be
-		// used to set the appropriate bits for the specified pin.
-		// Reference section 8.4.3 "GPIO port output speed register" for details.
-		register_offset = pin;
-
-		switch(ospeed)
-		{
-		case GPIO_OUTPUT_SPEED_LOW:
-			// x0: Low speed
-			port->OSPEEDR &= ~(0x1UL << register_offset);
-			break;
-
-		case GPIO_OUTPUT_SPEED_MEDIUM:
-			// 01: Medium speed
-			port->OSPEEDR |= (0x1UL << register_offset);
-			port->OSPEEDR &= ~(0x2UL << register_offset);
-			break;
-
-		case GPIO_OUTPUT_SPEED_HIGH:
-			// 11: High speed
-			port->OSPEEDR |= (0x3UL << register_offset);
-			break;
-
-		default:
-			// TODO: Add error handling
-			break;
-		}
-	}
-	else
-	{
-		// TODO: Add error handling
-	}
-}
-
-void GpioPin::pull_up_pull_down_configure(void)
-{
-	uint32_t register_offset = 0;
-
-	if(pin < 16)
-	{
-		// Get the register bit offset for the specified pin. This will be
-		// used to set the appropriate bits for the specified pin.
-		// Reference section 8.4.4 "GPIO port pull-up/pull-down register" for details.
-		register_offset = pin * 2;
-
-		switch(pupd)
-		{
-		case GPIO_PUPD_NONE:
-			// 00: No pull-up, pull-down
-			port->PUPDR &= ~(0x3UL << register_offset);
-			break;
-
-		case GPIO_PUPD_PULL_UP:
-			// 01: Pull-up
-			port->PUPDR |= (0x1UL << register_offset);
-			port->PUPDR &= ~(0x2UL << register_offset);
-			break;
-
-		case GPIO_PUPD_PULL_DOWN:
-			// 10: Pull-down
-			port->PUPDR &= ~(0x1UL << register_offset);
-			port->PUPDR |= (0x2UL << register_offset);
-			break;
-
-		case GPIO_PUPD_RESERVED:  // intentional fall-through
-		default:
-			// TODO: Add error handling
-			break;
-		}
-	}
-	else
-	{
-		// TODO: Add error handling
-	}
+    HAL_GPIO_TogglePin(port, pin);
 }
